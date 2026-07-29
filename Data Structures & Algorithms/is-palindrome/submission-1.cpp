48cce397656621013e32630002251cc0a7deda9a
lazzied <lazz.zied@gmail.com>
2026-07-29 14:39:52 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length()-1;
+        while (left < right){
+            if (!isalnum(s[left])) { left++; continue; }
+            else if (!isalnum(s[right])) { right--; continue; }
+            else if (tolower(s[left]) != tolower(s[right])) return false;
+            left ++;
+            right --;
+
+        }
+        return true;
+
+    }
+};
